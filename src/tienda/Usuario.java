cf3f91429aeca6d741e67c69a1868e0ef37e20c5
EduGuajardoProgra <eguajardo25@alumnos.utalca.cl>
2026-08-25 18:06:08 -0400

    Arraylist del carrito

--- src/tienda/Usuario.java
+++ src/tienda/Usuario.java
@@ -1,11 +1,20 @@
 package tienda;
 
-
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Usuario extends Cuenta {
+    private ArrayList<String> Carrito = new ArrayList<>();
 
     public Usuario(String nombre, String apellido, String contrasenia, String direccion, String pais, String comuna, String region, String correo, int numeroDeTelefono, Date fechadeNacimiento, String rut) {
         super(nombre, apellido, contrasenia, direccion, pais, comuna, region, correo, numeroDeTelefono, fechadeNacimiento, rut);
+    }
+
+    public ArrayList<String> getCarrito() {
+        return Carrito;
+    }
+
+    public void setCarrito(ArrayList<String> Carrito) {
+        this.Carrito = Carrito;
     }
 }
